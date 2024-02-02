fc84c67d2ea4cfc3c2a969fcdb3ea74091283056
initofa <mus2108000@gmail.com>
2024-02-02 19:01:52 +0700

    menambahkan koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_stok_barang";
+
+// Membuat koneksi ke database
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Memeriksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
